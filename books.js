e4790244b0d36a4ce5a451478d910dcf5845a188
Justin Crain <131824461+Jcrain5418@users.noreply.github.com>
2023-10-23 19:51:04 -0500

    ability to add books through js magic

--- books.js
+++ books.js
@@ -11,42 +11,54 @@ const confirmBtn = document.querySelector('.confirm');
 const form = document.querySelector('.form');
 const titleVal = document.querySelector('#title');
 const authorVal = document.querySelector('#author');
-const pageCount = document.querySelector('#pageCount')
-const readVal = document.querySelectorAll('.read')
+const pageCount = document.querySelector('#pageCount');
+
+//dom element for displaying books
+const tableBody = document.querySelector('.tbody');
 
 //array for book storage
 const myLibrary = [];
 
-function Books(title, author, pages, read) {
+function Books(title, author, pages) {
     this.title = title
 
     this.author = author
 
     this.pages = pages
-
-    this.read = read
 }
 
 function addBook() {
   let title = titleVal.value;
   let author = authorVal.value;
   let pages = pageCount.value;
-  let read = readVal.value;
   //adds new book
-  let newBook = new Books(title, author, pages, read)
+  let newBook = new Books(title, author, pages)
   myLibrary.push(newBook)
+}
+
+function displayBook() {
+  //tableBody.innerHtml = "";
+  myLibrary.forEach((book) => {
+    const bookHtml = 
+    `<tr>
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>
+      <label><input type="checkbox"></label>
+      </td>
+    </tr>`;
+    tableBody.insertAdjacentHTML("afterbegin",bookHtml)
+  })
 }
 
 function clearForm() {
   titleVal.value = "";
   authorVal.value = "";
   pageCount.value = "";
-  readVal.value = "";
 }
 
-//myLibrary.forEach((book)=> {
- // text.textContent = `${book.title} written by: ${book.author}`
-//})
+
 
 //show the dialog button button opens the dialog
 showBtn.addEventListener("click", () => {
@@ -61,6 +73,7 @@ closeBtn.addEventListener("click", () => {
 //prevents the confirm button from submitting the form
 confirmBtn.addEventListener("click", () => {
   addBook();
+  displayBook();
+  dialog.close();
   clearForm();
-  dialog.close()
 })
